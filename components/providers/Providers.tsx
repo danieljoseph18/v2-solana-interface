2d80cb2b085ec6eb1d0b88cf61f5537538062bba
danieljoseph18 <danieljoseph1805@gmail.com>
2024-12-21 13:44:28 +0400

    Added Privy To Providers

--- components/providers/Providers.tsx
+++ components/providers/Providers.tsx
@@ -5,15 +5,33 @@ import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { BottomNavProvider } from "@/contexts/BottomNavContext";
 import { AccountProvider } from "@/contexts/AccountContext";
+import { PrivyProvider } from "@privy-io/react-auth";
+import P3Logo from "@/app/assets/nav/nav-logo.png";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
   return (
-    <AccountProvider>
-      <BottomNavProvider>
-        <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
-      </BottomNavProvider>
-    </AccountProvider>
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      config={{
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: "dark",
+          accentColor: "#8210AA",
+          logo: P3Logo.src,
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: "users-without-wallets",
+        },
+      }}
+    >
+      <AccountProvider>
+        <BottomNavProvider>
+          <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+        </BottomNavProvider>
+      </AccountProvider>
+    </PrivyProvider>
   );
 }
